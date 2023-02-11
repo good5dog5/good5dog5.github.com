256ef33941249111f5577ad9dfdc0cef035b47e7
jordan <jordan.huang@ailabs.tw>
2023-02-11 15:24:24 +0800

    Site updated: 2023-02-11 15:24:24

--- js/custom.js
+++ js/custom.js
@@ -8,23 +8,28 @@ $(function () {
         messagingSenderId: '306104109190',
         appId: '1:306104109190:web:499f2ebcc235bf3f79a907',
     }
-    firebase.initializeApp(config);
+    firebase.initializeApp(config)
 
-    var database = firebase.database();
-    var oriUrl = window.location.host;
-    var curUrl = oriUrl + window.location.pathname;
+    var database = firebase.database()
+    var oriUrl = window.location.host
+    var curUrl = oriUrl + window.location.pathname
+
     function readVisits(url, selector) {
-    var db_key = decodeURI(url.replace(new RegExp('\\/|\\.', 'g'), "_"));
-        database.ref(db_key).once("value").then(function (result) {
-            var count = parseInt(result.val() || 0) + 1;
-            database.ref(db_key).set(count);
-            if (selector.length > 0) {
-                selector.html(count);
-            };
-        });
+        console.log(url);
+        var db_key = decodeURI(url.replace(new RegExp('\\/|\\.', 'g'), '_'))
+        database
+            .ref(db_key)
+            .once('value')
+            .then(function (result) {
+                var count = parseInt(result.val() || 0) + 1
+                database.ref(db_key).set(count)
+                if (selector.length > 0) {
+                    selector.html(count)
+                }
+            })
     }
-    readVisits(oriUrl, $("#visits .count"));
-    if (curUrl && curUrl != "_") {
-        readVisits("page/" + curUrl, $("#pageviews .count"));
+    readVisits(oriUrl, $('.article-view-count .count'))
+    if (curUrl && curUrl != '_') {
+        readVisits('page/' + curUrl, $('.article-view-count .count'))
     }
-});
+})
