7a73e906378e57bef4b3eb5bd8671f7ef841b21e
jordan <jordan.huang@ailabs.tw>
2023-02-11 12:21:28 +0800

    Site updated: 2023-02-11 12:21:27

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,13 @@
 $(function () {
     // Initialize Firebase
     var config = {
-        apiKey: "AIzaSyDE0Nb2sVXsR-uidm7A9VTveKCk5D_ZW2c",
-        authDomain: "hexo-3fdb4.firebaseapp.com",
-        databaseURL: "https://hexo-3fdb4.firebaseio.com",
-    };
+        authDomain: 'hexo-3fdb4.firebaseapp.com',
+        databaseURL: 'https://hexo-3fdb4.firebaseio.com',
+        projectId: 'hexo-3fdb4',
+        storageBucket: 'hexo-3fdb4.appspot.com',
+        messagingSenderId: '306104109190',
+        appId: '1:306104109190:web:499f2ebcc235bf3f79a907',
+    }
     firebase.initializeApp(config);
 
     var database = firebase.database();
@@ -13,7 +16,7 @@ $(function () {
     function readVisits(url, selector) {
     var db_key = decodeURI(url.replace(new RegExp('\\/|\\.', 'g'), "_"));
         database.ref(db_key).once("value").then(function (result) {
-            var coune = parseInt(result.val() || 0) + 1;
+            var count = parseInt(result.val() || 0) + 1;
             database.ref(db_key).set(count);
             if (selector.length > 0) {
                 selector.html(count);
